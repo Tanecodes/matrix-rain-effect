3b0e20fa716fce949b148b66f3bb728f45f6d914
Tanecodes <tanecodes@gmail.com>
2025-02-23 09:33:45 +1000

    implementing the matrix effect still need to create the function to create the animations

--- matrix.js
+++ matrix.js
@@ -1,16 +1,27 @@
-const enterButton = document.getElementById("enter-button");
+const enterButton = document.getElementById("enter-btn");
 const introText = document.getElementById("intro");
 
 enterButton.addEventListener("click",function(){
-
+// creating canvas for the matrix rain effect.
   const matrixCanvas = document.createElement("canvas");
   matrixCanvas.id = "matrix-canvas";
   document.body.appendChild(matrixCanvas);
 
+// setting the canvas width and height to fill the browser
   matrixCanvas.width = window.innerWidth;
   matrixCanvas.height = window.innerHeight;
 
+  // removing button and paragraph
   enterButton.style.display = "none";
   introText.style.display = "none";
-  console.log(matrixCanvas);
+
+  //setting variables for the matrix rain effect.
+  const matrixContext = matrixCanvas.getContext("2d");
+  const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const fontsize = 16;
+  const columns = Math.floor(matrixCanvas.width / fontsize);
+  const drop = new Array(columns).fill(0);
+
+  //creating matrix effect 
+
 });
